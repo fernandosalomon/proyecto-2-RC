b21d6a2d77ddfe07bb492933f31fd7ad1f1b92bc
Fernando F. Salomon <fernandosalomon1992@gmail.com>
2024-06-16 18:58:31 -0300

    feature(product-panel): add fuctionality to all the buttons

--- js/homeAdmin.js
+++ js/homeAdmin.js
@@ -7,9 +7,13 @@ const btnUsuariosNavbar = document.getElementById("navbarBtnUsuarios");
 const btnProductosNavbar = document.getElementById("navbarBtnProductos");
 const sidepanelPillProductos = document.getElementById("sidepanelPillProductos");
 const sidepanelPillUsuarios = document.getElementById("sidepanelPillUsuarios");
-const btnCrearProductos = document.getElementById("idBtnCrearProductos");
+const btnCrearProductos = document.getElementById("idBtnCrearProductosWrapper");
 const modalEditarUsuario = document.getElementById("idModalEditarUsuarioBody");
+const modalEditarProducto = document.getElementById("idModalEditarProductoBody");
+const modalCrearProducto = document.getElementById("idModalCrearProductoBody");
 const userRoles = ["usuario", "administrador"];
+const productos = JSON.parse(localStorage.getItem("productos")) || [];
+const productCategories = ["unisex", "man", "woman","children"];
 
 sidepanelPillProductos.innerText = `${productos.length}`;
 sidepanelPillUsuarios.innerText = `${usuarios.length}`;
@@ -63,10 +67,14 @@ const mainPanelProductos = () => {
       <td>$${producto.price}</td>
       <td>0</td>
       <td>
-        <button type="button" class="btn"><i class="bi bi-pencil-square"></i></button>
-        <button type="button" class="btn"><i class="bi bi-star"></i></button>
-        <button type="button" class="btn"><i class="bi bi-ban"></i></button>
-        <button type="button" class="btn btn-danger"><i class="bi bi-trash"></i></button>
+        <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#idModalEditarProducto" onclick=editarProducto(${producto.id})><i class="bi bi-pencil-square"></i></button>
+        <button type="button" class="btn" onclick=destacarProducto(${producto.id})>
+          <i class="bi text-warning ${producto.destacado? "bi-star-fill" : "bi-star"}"></i>
+        </button>
+        <button type="button" class="btn" onclick=deshabilitarHabilitarProducto(${producto.id})>
+          <i class="bi ${producto.bloqueado? "bi-exclamation":"bi-ban"}"></i>
+        </button>
+        <button type="button" class="btn btn-danger" onclick=eliminarProducto(${producto.id})><i class="bi bi-trash"></i></button>
       </td>
     </tr>
     `
@@ -170,3 +178,237 @@ const editarUsuario = (idUsuario) => {
     }
   });
 }
+
+// CRUD PRODUCTOS
+
+const deshabilitarHabilitarProducto = (idProducto) => {
+  const posicionProductoSeleccionado = productos.findIndex((producto) => 
+    producto.id === idProducto
+  );
+  if(confirm("¿Esta seguro que desea " + (productos[posicionProductoSeleccionado].bloqueado? "habilitar" : "deshabilitar") + " este producto?")){
+    productos[posicionProductoSeleccionado].bloqueado = !productos[posicionProductoSeleccionado].bloqueado;
+    localStorage.setItem("productos", JSON.stringify(productos));
+    location.reload();
+  }
+};
+
+const destacarProducto = (idProducto) => {
+  const posicionProductoSeleccionado = productos.findIndex((producto) => 
+    producto.id === idProducto
+  );
+
+  const productosDestacados= productos.filter( (producto) => producto.destacado);
+  
+
+  if(productosDestacados.length){
+   
+    const posicionProductoDestacado = productos.findIndex((producto) => producto.id === productosDestacados[0].id);
+    if(productosDestacados[0].id !== productos[posicionProductoSeleccionado].id){
+      if(confirm("¿Esta seguro que cambiar el producto destacado?")){
+        productos[posicionProductoSeleccionado].destacado = !productos[posicionProductoSeleccionado].destacado;
+        productos[posicionProductoDestacado].destacado = false;
+        localStorage.setItem("productos", JSON.stringify(productos));
+        location.reload();
+      }
+    }else{
+      alert("Debe haber al menos un producto destacado");
+    }
+    }else{
+      if(confirm("¿Esta seguro que desea " + (productos[posicionProductoSeleccionado].destacado? "quitar de destacados" : "destacar") + " este producto?")){
+        productos[posicionProductoSeleccionado].destacado = !productos[posicionProductoSeleccionado].destacado;
+        localStorage.setItem("productos", JSON.stringify(productos));
+        location.reload();
+      }
+    }
+}
+
+const eliminarProducto = (idProducto) => {
+  if(confirm("¿Esta seguro que desea eliminar este producto?")){
+    const newProductos = productos.filter( (producto) => producto.id !== idProducto );
+    localStorage.setItem("productos", JSON.stringify(newProductos));
+    location.reload();
+  }
+};
+
+const editarProducto = (idProducto) => {
+  const productoSeleccionado = productos.find((producto) => producto.id === idProducto);
+  modalEditarProducto.innerHTML = `
+     <div class="modal-content">
+            
+            <div class="d-flex justify-content-end">
+              <button type="button" class="btn-close m-3 fs-2" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-header object-cover d-flex flex-column">
+              <img src="${productoSeleccionado.image}" alt="${productoSeleccionado.title}" class="w-75">
+            </div>
+            <div class="modal-body">
+                    <form>
+                      <div class="container">
+                        <div class="row">
+                          <div class="mb-3 col-6">
+                            <label for="idNombreProductoModal" class="form-label fs-5">Nombre</label>
+                            <input type="text" class="form-control fs-4" id="idNombreProductoModal">
+                          </div>
+                          <div class="mb-3 col-6">
+                              <label for="idCategoriaProductoModal" class="form-label fs-5">Categoría</label>
+                              <select class="form-select fs-4" aria-label="categoriaProducto" id="idCategoriaProductoModal"></select>
+                          </div>
+                          <div class="mb-3 col-12">
+                              <label for="idImagenProductoModal" class="form-label fs-5">Imagen</label>
+                              <input type="url" class="form-control fs-4" id="idImagenProductoModal">
+                          </div>
+                          <div class="mb-3 col-12">
+                              <label for="idDescripcionProductoModal" class="form-label fs-5">Descripción</label>
+                              <textarea class="form-control" id="idDescripcionProductoModal"></textarea>
+                          </div>
+                          <div class="col-12 row">
+                            <div class="col-6">
+                              <div class="mb-3">
+                                <label for="idPrecioProductoModal" class="form-label fs-5">Precio</label>
+                                <div class="d-flex gap-2 aling-items-center">
+                                  <p class="fs-2 m-0">$</p>
+                                  <input type="text" class="form-control fs-4" id="idPrecioProductoModal">
+                                </div>
+                              </div>
+                            </div>
+                            <div class="col-6">
+                              <div class="mb-3">
+                                <label for="idCantidadStockProductoModal" class="form-label fs-5">Cantidad en Stock</label>
+                                <input type="text" class="form-control fs-4" id="idCantidadStockProductoModal">
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <button type="submit" class="btn btn-primary d-block w-100 mx-auto mt-5 fs-3"       id="idBtnEditarProductoModal">Editar Producto</button>
+                    </form>
+            </div>
+      </div>
+  `
+  const inputNombreProducto = document.getElementById("idNombreProductoModal");
+  const inputImagenProducto = document.getElementById("idImagenProductoModal");
+  const inputCategoriaProducto = document.getElementById("idCategoriaProductoModal");
+  const inputDescripcionProducto = document.getElementById("idDescripcionProductoModal");
+  const inputCantidadStockProducto = document.getElementById("idCantidadStockProductoModal");
+  const inputPrecioProducto = document.getElementById("idPrecioProductoModal");
+
+  inputCategoriaProducto.innerHTML = `
+    ${productCategories.map((category) => `<option ${productoSeleccionado.category === category? "selected": ""} value=${productCategories.findIndex((cat) => cat === category)}>${category}</option>`)}
+  `
+
+  inputNombreProducto.value = productoSeleccionado.title;
+  inputImagenProducto.value = productoSeleccionado.image;
+  inputDescripcionProducto.value = productoSeleccionado.description;
+  inputCantidadStockProducto.value = productoSeleccionado.cantidadStock;
+  inputPrecioProducto.value = productoSeleccionado.price;
+
+  const btnEditarProducto = document.getElementById("idBtnEditarProductoModal");
+  btnEditarProducto.addEventListener("click", (e) => {
+    e.preventDefault();
+    productoSeleccionado.title = inputNombreProducto.value;
+    productoSeleccionado.image = inputImagenProducto.value;
+    productoSeleccionado.description = inputDescripcionProducto.value;
+    productoSeleccionado.cantidadStock = inputCantidadStockProducto.value;
+    productoSeleccionado.price = inputPrecioProducto.value;
+    productoSeleccionado.category = inputCategoriaProducto.value;
+    
+    const posicionProductoSeleccionado = productos.findIndex( (producto) => producto.id === productoSeleccionado.id);
+    
+    if(confirm("¿Está seguro que desea modificar la información de este producto?")){
+      productos[posicionProductoSeleccionado] = productoSeleccionado;
+      console.log(productos);
+      localStorage.setItem("productos", JSON.stringify(productos));
+      location.reload();
+    }
+  });
+}
+
+const crearProducto = () => {
+  modalCrearProducto.innerHTML = `
+     <div class="modal-content">
+            
+            <div class="d-flex justify-content-end">
+              <button type="button" class="btn-close m-3 fs-2" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-header object-cover d-flex flex-column">
+              <h2 class="text-center">Agregar un nuevo producto</h2>
+            </div>
+            <div class="modal-body">
+                    <form>
+                      <div class="container">
+                        <div class="row">
+                          <div class="mb-3 col-6">
+                            <label for="idNombreProductoModal" class="form-label fs-5">Nombre</label>
+                            <input type="text" class="form-control fs-4" id="idNombreProductoModal">
+                          </div>
+                          <div class="mb-3 col-6">
+                              <label for="idCategoriaProductoModal" class="form-label fs-5">Categoría</label>
+                              <select class="form-select fs-4" aria-label="categoriaProducto" id="idCategoriaProductoModal"></select>
+                          </div>
+                          <div class="mb-3 col-12">
+                              <label for="idImagenProductoModal" class="form-label fs-5">Imagen</label>
+                              <input type="url" class="form-control fs-4" id="idImagenProductoModal">
+                          </div>
+                          <div class="mb-3 col-12">
+                              <label for="idDescripcionProductoModal" class="form-label fs-5">Descripción</label>
+                              <textarea class="form-control" id="idDescripcionProductoModal"></textarea>
+                          </div>
+                          <div class="col-12 row">
+                            <div class="col-6">
+                              <div class="mb-3">
+                                <label for="idPrecioProductoModal" class="form-label fs-5">Precio</label>
+                                <div class="d-flex gap-2 aling-items-center">
+                                  <p class="fs-2 m-0">$</p>
+                                  <input type="text" class="form-control fs-4" id="idPrecioProductoModal">
+                                </div>
+                              </div>
+                            </div>
+                            <div class="col-6">
+                              <div class="mb-3">
+                                <label for="idCantidadStockProductoModal" class="form-label fs-5">Cantidad en Stock</label>
+                                <input type="text" class="form-control fs-4" id="idCantidadStockProductoModal">
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <button type="submit" class="btn btn-primary d-block w-100 mx-auto mt-5 fs-3"       id="idBtnEditarProductoModal">Crear Nuevo Producto</button>
+                    </form>
+            </div>
+      </div>
+  `
+  const inputNombreProducto = document.getElementById("idNombreProductoModal");
+  const inputImagenProducto = document.getElementById("idImagenProductoModal");
+  const inputCategoriaProducto = document.getElementById("idCategoriaProductoModal");
+  const inputDescripcionProducto = document.getElementById("idDescripcionProductoModal");
+  const inputCantidadStockProducto = document.getElementById("idCantidadStockProductoModal");
+  const inputPrecioProducto = document.getElementById("idPrecioProductoModal");
+
+  inputCategoriaProducto.innerHTML = `
+    ${productCategories.map((category) => `<option value=${productCategories.findIndex((cat) => cat === category)}>${category}</option>`)}
+  `
+
+  const btnEditarProducto = document.getElementById("idBtnEditarProductoModal");
+  btnEditarProducto.addEventListener("click", (e) => {
+    e.preventDefault();
+    const nuevoProducto = {};
+    nuevoProducto.id = productos[productos.length - 1].id + 1;
+    nuevoProducto.title = inputNombreProducto.value;
+    nuevoProducto.image = inputImagenProducto.value;
+    nuevoProducto.description = inputDescripcionProducto.value;
+    nuevoProducto.cantidadStock = inputCantidadStockProducto.value;
+    nuevoProducto.price = inputPrecioProducto.value;
+    nuevoProducto.category = inputCategoriaProducto.value;
+    nuevoProducto.bloqueado = false;
+    nuevoProducto.destacado = false;
+    
+    if(confirm("¿Está seguro que desea crear un nuevo producto?")){
+      productos.push(nuevoProducto);
+      localStorage.setItem("productos", JSON.stringify(productos));
+      location.reload();
+    }
+  });
+}
+
+const btnCrearProducto = document.getElementById("idBtnCrearProductos");
+btnCrearProducto.addEventListener("click", crearProducto); 
