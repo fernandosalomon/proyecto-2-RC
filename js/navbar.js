b58106aeaaa9a07f1418c4ac9d0076ff0c0949a7
fernandosalomon <fernandosalomon1992@gmail.com>
2024-06-19 19:52:14 -0300

    style(cards): add style to cards

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,21 @@
 const userOptions = document.getElementById("userOptions");
 // const usuario = JSON.parse(sessionStorage.getItem("usuario")) || "";
 
-if(usuario === ""){
+if (usuario === "") {
   userOptions.innerHTML = `
   <button type="button" class="btn-1" id="idBtnIngresar">Ingresar</button>
-  `
-}else{
+  `;
+
+  const btnIngresar = document.getElementById("idBtnIngresar");
+
+  btnIngresar.addEventListener("click", () => {
+    if (location.pathname.search("pages") === -1) {
+      location.href = "./pages/login-registro.html";
+    } else {
+      location.href = "./login-registro.html";
+    }
+  });
+} else {
   userOptions.innerHTML = `
   <ul class="navbar-nav">
     <li class="nav-item dropdown">
@@ -19,29 +29,15 @@ if(usuario === ""){
       </ul>
     </li>
   </ul>  
-  `
-}
+  `;
+  if (usuario !== "") {
+    const btnCerrarSesion = document.getElementById("idBtnCerrarSesion");
 
-if(usuario !== ""){
-const btnCerrarSesion = document.getElementById("idBtnCerrarSesion");
-
-btnCerrarSesion.addEventListener("click", () => {
-  sessionStorage.removeItem("usuario");
-  setTimeout(() => {
-    location.href = "../index.html";
-  }, 1000);
-});
-}
-
-const btnIngresar = document.getElementById("idBtnIngresar");
-
-btnIngresar.addEventListener("click", () => {
-
-  if(location.pathname.search("pages") === -1){
-    location.href = "./pages/login-registro.html";
-  }else{
-    location.href = "./login-registro.html";
+    btnCerrarSesion.addEventListener("click", () => {
+      sessionStorage.removeItem("usuario");
+      setTimeout(() => {
+        location.href = "../index.html";
+      }, 1000);
+    });
   }
-
-});
-
+}
--- js/productos.js
+++ js/productos.js
@@ -4,13 +4,14 @@ mainBody.innerHTML = productos
   .map(
     (producto) =>
       `
-<div class="card col-12 col-md-6 col-lg-4" style="width: 32rem;">
-  <img src="${producto.image}" class="card-img-top" alt="${producto.title}">
+<div class="card col-12 col-md-6 col-lg-4 p-0 border-0 card-producto" style="width: 32rem;">
+  <div class="img-container">
+    <img src="${producto.image}" class="card-img-top" alt="${producto.title}">
+  </div>
   <div class="card-body">
     <h5 class="card-title">${producto.title}</h5>
-    <p class="card-text">${producto.category}</p>
-    <p class="card-text">$${producto.price}</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <p class="card-category">${producto.category}</p>
+    <p class="card-price">$${producto.price}</p>
   </div>
 </div>
 `
