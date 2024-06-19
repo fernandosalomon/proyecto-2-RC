5eab654136a592fda24b01ed2688166b688319c0
fernandosalomon <fernandosalomon1992@gmail.com>
2024-06-19 19:23:32 -0300

    style(cards): add grid layout

--- js/productos.js
+++ js/productos.js
@@ -1,18 +1,18 @@
 const mainBody = document.getElementById("idMainBody");
 
-mainBody.innerHTML = productos.map( (producto) => 
-`
-<div class="card" style="width: 18rem;">
+mainBody.innerHTML = productos
+  .map(
+    (producto) =>
+      `
+<div class="card col-12 col-md-6 col-lg-4" style="width: 32rem;">
   <img src="${producto.image}" class="card-img-top" alt="${producto.title}">
   <div class="card-body">
     <h5 class="card-title">${producto.title}</h5>
-    <p class="card-text">$${producto.category}</p>
+    <p class="card-text">${producto.category}</p>
     <p class="card-text">$${producto.price}</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
 `
-
-
-
-);
+  )
+  .join("");
