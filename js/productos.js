f10175c139d475577668bc7f89669816fcea71c3
fernandosalomon <fernandosalomon1992@gmail.com>
2024-06-21 20:47:26 -0300

    feature(search-bar): add searchbar for products

--- js/productos.js
+++ js/productos.js
@@ -15,6 +15,9 @@ const btnRestablecerFiltros = document.getElementById(
   "idBtnRestablecerFiltros"
 );
 const absoluteMaxPrice = 500;
+const btnBuscarProducto = document.getElementById("idBtnBuscarProducto");
+const inputBuscarProducto = document.getElementById("idInputBuscarProducto");
+const mainHeaderOptions = document.getElementById("idMainHeaderOptions");
 
 mainBody.innerHTML = productos
   .map(
@@ -152,4 +155,37 @@ btnRestablecerFiltros.addEventListener("click", () => {
     )
     .join("");
   closeModalFiltro.click();
+});
+
+btnBuscarProducto.addEventListener("click", () => {
+  inputBuscarProducto.classList.remove("d-none");
+  inputBuscarProducto.focus();
+  inputBuscarProducto.addEventListener("input", (e) => {
+    nombreProductoBuscar = e.target.value.trim().toLowerCase();
+    mainBody.innerHTML = productos
+      .filter(
+        (producto) =>
+          producto.title.trim().toLowerCase().search(nombreProductoBuscar) !==
+          -1
+      )
+      .map(
+        (producto) =>
+          `
+          <div class="card col-12 col-md-6 col-lg-4 p-0 border-0 card-producto" style="width: 32rem;">
+            <div class="img-container">
+              <img src="${producto.image}" class="card-img-top" alt="${producto.title}">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">${producto.title}</h5>
+              <p class="card-category">${producto.category}</p>
+              <p class="card-price">$${producto.price}</p>
+            </div>
+          </div>
+          `
+      )
+      .join("");
+  });
+  mainHeaderOptions.addEventListener("focusout", () => {
+    inputBuscarProducto.classList.add("d-none");
+  });
 });
