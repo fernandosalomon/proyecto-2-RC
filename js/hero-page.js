9309edf1816be33fb24d5997e52a56fd12eace2e
fernandosalomon <fernandosalomon1992@gmail.com>
2024-06-23 09:12:40 -0300

    feature(btn-heropage): add functionality to btn in heropage

--- js/hero-page.js
+++ js/hero-page.js
@@ -1,13 +1,32 @@
 const imgProductoDestacado = document.getElementById("idImgProductoDestacado");
 const titleProductoDestacado = document.getElementById("idHeroProductTitle");
 const priceProductoDestacado = document.getElementById("idHeroProductPrize");
+const btnEmpezarAComprar = document.getElementById("idBtnEmpezarAComprar");
+const btnComprarProductoDestacado = document.getElementById(
+  "idBtnComprarProductoDestacado"
+);
 // const productos = JSON.parse(localStorage.getItem("productos")) || [];
 
-const productoDestacado = productos.filter( (producto) => producto.destacado);
+const productoDestacado = productos.filter((producto) => producto.destacado);
 
-const productoDestacadoImgURL = "./" + productoDestacado[0].image.split("/").splice(1).join("/");
+const productoDestacadoImgURL =
+  "./" + productoDestacado[0].image.split("/").splice(1).join("/");
 
 imgProductoDestacado.src = productoDestacadoImgURL;
 imgProductoDestacado.alt = productoDestacado[0].title;
 titleProductoDestacado.innerText = productoDestacado[0].title;
 priceProductoDestacado.innerHTML = `$${productoDestacado[0].price}`;
+
+btnEmpezarAComprar.addEventListener("click", () => {
+  if (usuario) {
+    location.href = "./pages/productos.html";
+  } else {
+    location.href = "./pages/login-registro.html";
+  }
+});
+
+btnComprarProductoDestacado.addEventListener(
+  "click",
+  () =>
+    (location.href = `./pages/detalleProducto.html?id=${productoDestacado[0].id}`)
+);
