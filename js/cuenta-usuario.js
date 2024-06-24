e287b408965e4213333afa1d266c2853fff5897f
fernandosalomon <fernandosalomon1992@gmail.com>
2024-06-23 23:34:23 -0300

    feature(cuenta-usuario): add modify user section

--- js/cuenta-usuario.js
+++ js/cuenta-usuario.js
@@ -5,9 +5,6 @@ const ventanaLateral = document.getElementById("idVentanaLateralCU");
 const btnBarraLateralPerfil = document.getElementById(
   "idBtnBarraLateralPerfil"
 );
-const btnBarraLateralDirecciones = document.getElementById(
-  "idBtnBarraLateralDirecciones"
-);
 const btnBarraLateralPedidos = document.getElementById(
   "idBtnBarraLateralPedidos"
 );
@@ -92,3 +89,216 @@ const agregarProductoCarrito = (idProducto) => {
     alert("El producto ya se encuenctra en el carrito");
   }
 };
+
+btnBarraLateralPerfil.addEventListener("click", () => {
+  ventanaLateral.innerHTML = `
+    <form class="row g-3 w-75">
+          <div class="col-12">
+            <label for="inputNombreUsuario" class="form-label fs-3">Nombre</label>
+            <input type="text" class="form-control fs-3" id="inputNombreUsuario" value="${
+              usuario.nombre
+            }">
+            <div id="idNombreErrorMsg" class="text-danger fs-4 ms-2 mt-2 d-none">El Nombre ingresado no es valido</div>
+          </div>
+          <div class="col-md-6">
+            <label for="inputEmailUsuario" class="form-label fs-3">Email</label>
+            <input type="email" class="form-control fs-3" id="inputEmailUsuario" value="${
+              usuario.email
+            }">
+            <div id="idEmailRegistroErrorMsg" class="text-danger fs-4 ms-2 mt-2 d-none">El formato de e-mail ingresado no es válido (ej. juan@perez.com)</div>
+           
+          </div>
+          <div class="col-md-6">
+            <label for="inputPasswordUsuario" class="form-label fs-3">Contraseña</label>
+            <input type="password" class="form-control fs-3" id="inputPasswordUsuario" placeholder="*******">
+            <div id="idPasswordErrorMsg" class="text-danger fs-4 ms-2 mt-2 d-none">La contraseña debe contener entre 8 y 16 caracteres, al menos una minuscula, una mayuscula y un caracter especial (%@_!)</div>
+          </div>
+          <div class="col-12">
+            <label for="inputDireccionUsuario" class="form-label fs-3">Dirección</label>
+            <input type="text" class="form-control fs-3" id="inputDireccionUsuario" value="${
+              usuario.direccion !== undefined ? usuario.direccion : ""
+            }">
+            <div id="idDireccionErrorMsg" class="text-danger fs-4 ms-2 mt-2 d-none">La dirección ingresada no es válida.</div>
+          </div>
+          <div class="col-md-6">
+            <label for="inputCiudadUsuario" class="form-label fs-3">Ciudad</label>
+            <input type="text" class="form-control fs-3" id="inputCiudadUsuario" value="${
+              usuario.ciudad !== undefined ? usuario.ciudad : ""
+            }">
+            <div id="idCiudadErrorMsg" class="text-danger fs-4 ms-2 mt-2 d-none">La ciudad ingresada no es válida.</div>
+          </div>
+          <div class="col-md-4">
+            <label for="inputProvinciaUsuario" class="form-label fs-3">Provincia</label>
+            <input type="text" class="form-control fs-3" id="inputProvinciaUsuario" value="${
+              usuario.provincia !== undefined ? usuario.provincia : ""
+            }">
+            <div id="idProvinciaErrorMsg" class="text-danger fs-4 ms-2 mt-2 d-none">La provincia ingresada no es válida.</div>
+          </div>
+          <div class="col-md-2">
+            <label for="inputCPUsuario" class="form-label fs-3">CP</label>
+            <input type="text" class="form-control fs-3" id="inputCPUsuario" value="${
+              usuario.cp !== undefined ? usuario.cp : ""
+            }">
+            <div id="idCPErrorMsg" class="text-danger fs-4 ms-2 mt-2 d-none">El código postal ingresado no es válida.</div>
+          </div>
+          <div class="col-12">
+            <label for="inputPasswordActualUsuario" class="form-label fs-4">Por razones de seguridad deberá ingresar su contraseña para modificar sus datos personales</label>
+            <input type="password" class="form-control w-50 fs-3" id="inputPasswordActualUsuario">
+          </div>
+          <div class="col-12">
+            <button type="submit" class="btn btn-primary fs-2" id="idBtnModificarDatosUsuario">Modificar Datos Personales</button>
+          </div>
+        </form>
+  `;
+  const inputNombreUsuario = document.getElementById("inputNombreUsuario");
+  const inputEmailUsuario = document.getElementById("inputEmailUsuario");
+  const inputPasswordUsuario = document.getElementById("inputPasswordUsuario");
+  const inputDireccionUsuario = document.getElementById(
+    "inputDireccionUsuario"
+  );
+  const inputCiudadUsuario = document.getElementById("inputCiudadUsuario");
+  const inputProvinciaUsuario = document.getElementById(
+    "inputProvinciaUsuario"
+  );
+  const inputCPUsuario = document.getElementById("inputCPUsuario");
+  const inputPasswordActualUsuario = document.getElementById(
+    "inputPasswordActualUsuario"
+  );
+
+  const emailErrorMsg = document.getElementById("idEmailRegistroErrorMsg");
+  const nombreErrorMsg = document.getElementById("idNombreErrorMsg");
+  const passwordErrorMsg = document.getElementById("idPasswordErrorMsg");
+  const direccionErrorMsg = document.getElementById("idDireccionErrorMsg");
+  const ciudadErrorMsg = document.getElementById("idCiudadErrorMsg");
+  const provinciaErrorMsg = document.getElementById("idProvinciaErrorMsg");
+  const CPErrorMsg = document.getElementById("idCPErrorMsg");
+
+  const validarFormulario = () => {
+    let validated = [0, 0, 0, 0, 0, 0, 0];
+
+    if (/^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+$/.test(inputNombreUsuario.value)) {
+      validated[0] = 1;
+      inputNombreUsuario.classList.remove("is-invalid");
+      inputNombreUsuario.classList.add("is-valid");
+    } else {
+      nombreErrorMsg.classList.remove("d-none");
+      inputNombreUsuario.classList.remove("is-valid");
+      inputNombreUsuario.classList.add("is-invalid");
+    }
+
+    if (
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
+        inputEmailUsuario.value
+      )
+    ) {
+      validated[1] = 1;
+      inputEmailUsuario.classList.remove("is-invalid");
+      inputEmailUsuario.classList.add("is-valid");
+    } else {
+      emailErrorMsg.classList.remove("d-none");
+      inputEmailUsuario.classList.add("is-invalid");
+      inputEmailUsuario.classList.remove("is-valid");
+    }
+
+    if (
+      /^(?=.*[A-Z])(?=.*[a-z])(?=.*[#\!@$%]).{8,16}$/.test(
+        inputPasswordUsuario.value
+      ) ||
+      inputPasswordUsuario.value === ""
+    ) {
+      validated[2] = 1;
+      inputPasswordUsuario.classList.remove("is-invalid");
+      inputPasswordUsuario.classList.add("is-valid");
+    } else {
+      passwordErrorMsg.classList.remove("d-none");
+      inputPasswordUsuario.classList.add("is-invalid");
+      inputPasswordUsuario.classList.remove("is-valid");
+    }
+
+    if (/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ\s]+$/.test(inputDireccionUsuario.value)) {
+      validated[3] = 1;
+      inputDireccionUsuario.classList.remove("is-invalid");
+      inputDireccionUsuario.classList.add("is-valid");
+    } else {
+      direccionErrorMsg.classList.remove("d-none");
+      inputDireccionUsuario.classList.remove("is-valid");
+      inputDireccionUsuario.classList.add("is-invalid");
+    }
+
+    if (/^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+$/.test(inputCiudadUsuario.value)) {
+      validated[4] = 1;
+      inputCiudadUsuario.classList.remove("is-invalid");
+      inputCiudadUsuario.classList.add("is-valid");
+    } else {
+      ciudadErrorMsg.classList.remove("d-none");
+      inputCiudadUsuario.classList.remove("is-valid");
+      inputCiudadUsuario.classList.add("is-invalid");
+    }
+
+    if (/^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+$/.test(inputProvinciaUsuario.value)) {
+      validated[5] = 1;
+      inputProvinciaUsuario.classList.remove("is-invalid");
+      inputProvinciaUsuario.classList.add("is-valid");
+    } else {
+      provinciaErrorMsg.classList.remove("d-none");
+      inputProvinciaUsuario.classList.remove("is-valid");
+      inputProvinciaUsuario.classList.add("is-invalid");
+    }
+
+    if (/^[a-zA-Z0-9]+$/.test(inputCPUsuario.value)) {
+      validated[6] = 1;
+      inputCPUsuario.classList.remove("is-invalid");
+      inputCPUsuario.classList.add("is-valid");
+    } else {
+      CPErrorMsg.classList.remove("d-none");
+      inputCPUsuario.classList.remove("is-valid");
+      inputCPUsuario.classList.add("is-invalid");
+    }
+
+    if (
+      inputPasswordActualUsuario.value === usuario.password &&
+      validated.reduce((a, b) => a + b, 0) === 7
+    ) {
+      usuarioSeleccionado = usuarios.filter(
+        (usuarioDB) => usuarioDB.id === usuario.id
+      );
+
+      usuarioSeleccionado[0].nombre = inputNombreUsuario.value;
+      usuarioSeleccionado[0].email = inputEmailUsuario.value;
+      inputPasswordUsuario.value !== inputPasswordActualUsuario.value
+        ? (usuarioSeleccionado[0].password = inputPasswordUsuario.value)
+        : "";
+      usuarioSeleccionado[0].direccion = inputDireccionUsuario.value;
+      usuarioSeleccionado[0].ciudad = inputCiudadUsuario.value;
+      usuarioSeleccionado[0].provincia = inputProvinciaUsuario.value;
+      usuarioSeleccionado[0].cp = inputCPUsuario.value;
+
+      console.log(usuarioSeleccionado);
+
+      posicionUsuarioSeleccionado = usuarios.findIndex(
+        (usuario) => usuario.id === usuarioSeleccionado.id
+      );
+
+      if (
+        confirm(
+          "¿Está seguro que desea modificar la información de este usuario?"
+        )
+      ) {
+        usuarios[posicionUsuarioSeleccionado] = usuarioSeleccionado;
+        localStorage.setItem("usuarios", JSON.stringify(usuarios));
+        location.reload();
+      }
+    } else {
+      alert("La contraseña no es correcta.");
+    }
+    console.log(usuario);
+  };
+
+  const btnModificarDatosUsuario = document.getElementById(
+    "idBtnModificarDatosUsuario"
+  );
+  btnModificarDatosUsuario.addEventListener("click", (e) => {
+    e.preventDefault();
+    validarFormulario();
+  });
+});
